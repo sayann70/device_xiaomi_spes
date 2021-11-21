4ff8b97f99e629bc78d8c729226c85bd3de8fc1a
Tim Zimmermann <tim@linux4.de>
2021-11-21 20:17:51 +0100

    spes: Uprev audio HAL to V7
    Updated audio policy by running:
    
    'hardware/interfaces/audio/7.0/config/update_audio_policy_config.sh \
    device/xiaomi/spes/configs/audio/audio_policy_configuration.xml 7.0'
    
    with some manual intervention.
    
    Change-Id: Iede32c176742e89577365341732a86f6ecaee514
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- device.mk
+++ device.mk
@@ -106,12 +106,11 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
-    android.hardware.audio.sounddose-vendor-impl \
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
-   android.hardware.soundtrigger@2.3-impl
+    android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
