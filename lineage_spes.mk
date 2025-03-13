eecc1a40844da896e9b9351fe11756253795002a
sayann70 <sayansarkarda2@gmail.com>
2025-03-13 08:43:10 +0530

    spes: Bringup Matrixxx
    Signed-off-by: sayann70 <sayansarkarda2@gmail.com>

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-# Inherit some common Project-Flare stuff.
-$(call inherit-product, vendor/flare/config/common_full_phone.mk)
+# Inherit some common Matrixx stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Product Specifics
-PRODUCT_NAME := flare_spes
+PRODUCT_NAME := lineage_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
@@ -26,13 +26,11 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 # Boot animaton
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Flare Official Stuff
-FLARE_BUILD_TYPE := OFFICIAL
-FLARE_MAINTAINER := sayann70
-TARGET_FACE_UNLOCK_SUPPORTED := true
+# To Build Pixel launcher
+TARGET_INCLUDE_NEXUS := true
 
 # Gapps
-WITH_GAPPS := true
+WITH_GMS := true
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
