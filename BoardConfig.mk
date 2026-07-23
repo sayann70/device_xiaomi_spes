a2b0df6044cbd76f1407ef396dd57022827eb4d6
sayann70 <sayansarkarda2@gmail.com>
2026-07-23 12:34:16 +0000

    spes: Clean-up VINTF compatibility matrix entries
    Remove the redundant/lineage-specific VINTF compatibility matrix
    references from BoardConfig.mk:
    
    - Drop the duplicate hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
      line and the vendor/lineage/config/device_framework_matrix.xml
      reference, keeping a single consolidated entry for the Xiaomi
      vendor framework compatibility matrix.
    - Remove the now-unused  block
      that appended a lineage-specific compatibility matrix
      (framework_compatibility_matrix-lineage.xml), since this build no
      longer targets the lineage_spes product.
    
    This trims stale Lineage-specific VINTF wiring left over from prior
    device tree iterations, keeping BoardConfig.mk aligned with the
    current (non-Lineage) build target.
    
    Signed-off-by: sayann70 <sayansarkarda2@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,14 +128,9 @@ TARGET_OTA_ASSERT_DEVICE := spes,spesn
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
-ifeq ($(PRODUCT_NAME), lineage_spes)
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix-lineage.xml
-endif
 ODM_MANIFEST_SKUS += k7tn
 ODM_MANIFEST_K7TN_FILES := $(DEVICE_PATH)/configs/vintf/manifest_k7tn.xml
 
