8afb7f1775c74bbd26fc0349df40a587e66d23d5
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-08-02 14:42:42 +0000

    spes:dalvik: fixup! fixing dalvik|art|usap tuning regressions
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>
    Signed-off-by: sayann70 <sayansarkarda2@gmail.com>

--- libinit/libinit_dalvik_heap.cpp
+++ libinit/libinit_dalvik_heap.cpp
@@ -15,90 +15,46 @@
 #define HEAPMINFREE_PROP "dalvik.vm.heapminfree"
 #define HEAPMAXFREE_PROP "dalvik.vm.heapmaxfree"
 #define HEAPTARGETUTILIZATION_PROP "dalvik.vm.heaptargetutilization"
-#define FOREGROUND_HEAP_GROWTH_MULTIPLIER_PROP "dalvik.vm.foreground-heap-growth-multiplier"
-#define ENABLE_TIME_BASED_GC_TRIGGER_PROP "dalvik.vm.enable_time_based_gc_trigger"
-#define USEJIT_PROP "dalvik.vm.usejit"
-#define JITMAXSIZE_PROP "dalvik.vm.jitmaxsize"
-#define JITINITIALSIZE_PROP "dalvik.vm.jitinitialsize"
-#define JITTHRESHOLD_PROP "dalvik.vm.jitthreshold"
-#define PARALLEL_IMAGE_LOADING_PROP "dalvik.vm.parallel-image-loading"
 #define MADVISE_VDEXFILE_SIZE_PROP "dalvik.vm.madvise.vdexfile.size"
 #define MADVISE_ODEXFILE_SIZE_PROP "dalvik.vm.madvise.odexfile.size"
-#define USAP_POOL_ENABLED_PROP "dalvik.vm.usap_pool_enabled"
-#define USAP_POOL_SIZE_MIN_PROP "dalvik.vm.usap_pool_size_min"
-#define USAP_POOL_SIZE_MAX_PROP "dalvik.vm.usap_pool_size_max"
-#define USAP_REFILL_THRESHOLD_PROP "dalvik.vm.usap_refill_threshold"
-#define USAP_REFILL_DELAY_MS_PROP "dalvik.vm.usap_pool_refill_delay_ms"
-#define PINNER_QUOTA "persist.sys.pinner.quota_pct"
+#define MADVISE_ARTFILE_SIZE_PROP "dalvik.vm.madvise.artfile.size"
 
 #define GB(b) (b * 1024ull * 1024 * 1024)
 
 static const dalvik_heap_info_t dalvik_heap_info_8192 = {
     .heapstartsize = "16m",
-    .heapgrowthlimit = "384m",
+    .heapgrowthlimit = "256m",
     .heapsize = "512m",
-    .heapminfree = "4m",
-    .heapmaxfree = "64m",
-    .heaptargetutilization = "0.75",
-    .foreground_heap_growth_multiplier = "1.0",
-    .enable_time_based_gc_trigger = "true",
-    .usejit = "true",
-    .jitmaxsize = "256m",
-    .jitinitialsize = "32m",
-    .jitthreshold = "10000",
-    .parallel_image_loading = "true",
-    .madvise_vdexfile_size = "157286400",
-    .madvise_odexfile_size = "157286400",
-    .usap_pool_enabled =  "true",
-    .usap_pool_size_min = "1",
-    .usap_pool_size_max = " 3",
-    .usap_refill_threshold = "1",
-    .usap_pool_refill_delay_ms = "3000",
+    .heapminfree = "8m",
+    .heapmaxfree = "32m",
+    .heaptargetutilization = "0.5",
+    .madvise_vdexfile_size = "31457280",
+    .madvise_odexfile_size = "31457280",
+    .madvise_artfile_size = "0",
 };
 
 static const dalvik_heap_info_t dalvik_heap_info_6144 = {
     .heapstartsize = "16m",
     .heapgrowthlimit = "256m",
     .heapsize = "512m",
-    .heapminfree = "4m",
-    .heapmaxfree = "48m",
-    .heaptargetutilization = "0.75",
-    .foreground_heap_growth_multiplier = "1.0",
-    .enable_time_based_gc_trigger = "true",
-    .usejit = "true",
-    .jitmaxsize = "256m",
-    .jitinitialsize = "32m",
-    .jitthreshold = "10000",
-    .parallel_image_loading = "true",
-    .madvise_vdexfile_size = "104857600",
-    .madvise_odexfile_size = "104857600",
-    .usap_pool_enabled =  "true",
-    .usap_pool_size_min = "1",
-    .usap_pool_size_max = " 2",
-    .usap_refill_threshold = "1",
-    .usap_pool_refill_delay_ms = "3000",
+    .heapminfree = "8m",
+    .heapmaxfree = "32m",
+    .heaptargetutilization = "0.5",
+    .madvise_vdexfile_size = "31457280",
+    .madvise_odexfile_size = "31457280",
+    .madvise_artfile_size = "0",
 };
 
 static const dalvik_heap_info_t dalvik_heap_info_4096 = {
-    .heapstartsize = "12m",
-    .heapgrowthlimit = "256m",
+    .heapstartsize = "8m",
+    .heapgrowthlimit = "192m",
+    .heaptargetutilization = "0.6",
     .heapsize = "512m",
-    .heapminfree = "4m",
-    .heapmaxfree = "32m",
-    .heaptargetutilization = "0.75",
-    .foreground_heap_growth_multiplier = "0.5",
-    .usejit = "true",
-    .jitmaxsize = "128m",
-    .jitinitialsize = "16m",
-    .jitthreshold = "15000",
-    .madvise_vdexfile_size = "104857600",
-    .madvise_odexfile_size = "104857600",
-    .usap_pool_enabled =  "true",
-    .usap_pool_size_min = "1",
-    .usap_pool_size_max = " 2",
-    .usap_refill_threshold = "1",
-    .usap_pool_refill_delay_ms = "3000",
-    .pinner_quota = "8",
+    .heapminfree = "8m",
+    .heapmaxfree = "16m",
+    .madvise_vdexfile_size = "31457280",
+    .madvise_odexfile_size = "31457280",
+    .madvise_artfile_size = "0",
 };
 
 void set_dalvik_heap() {
@@ -120,23 +76,7 @@ void set_dalvik_heap() {
     property_override(HEAPTARGETUTILIZATION_PROP, dhi->heaptargetutilization);
     property_override(HEAPMINFREE_PROP, dhi->heapminfree);
     property_override(HEAPMAXFREE_PROP, dhi->heapmaxfree);
-    property_override(FOREGROUND_HEAP_GROWTH_MULTIPLIER_PROP, dhi->foreground_heap_growth_multiplier);
-    property_override(USEJIT_PROP, dhi->usejit);
-    property_override(JITMAXSIZE_PROP, dhi->jitmaxsize);
-    property_override(JITINITIALSIZE_PROP, dhi->jitinitialsize);
-    property_override(JITTHRESHOLD_PROP, dhi->jitthreshold);
     property_override(MADVISE_VDEXFILE_SIZE_PROP, dhi->madvise_vdexfile_size);
     property_override(MADVISE_ODEXFILE_SIZE_PROP, dhi->madvise_odexfile_size);
-    property_override(USAP_POOL_ENABLED_PROP, dhi->usap_pool_enabled);
-    property_override(USAP_POOL_SIZE_MIN_PROP, dhi->usap_pool_size_min);
-    property_override(USAP_POOL_SIZE_MAX_PROP, dhi->usap_pool_size_max);
-    property_override(USAP_REFILL_THRESHOLD_PROP, dhi->usap_refill_threshold);
-    property_override(USAP_REFILL_DELAY_MS_PROP, dhi->usap_pool_refill_delay_ms);
-  if (sys.totalram > GB(5)) {
-    property_override(ENABLE_TIME_BASED_GC_TRIGGER_PROP, dhi->enable_time_based_gc_trigger);
-    property_override(PARALLEL_IMAGE_LOADING_PROP, dhi->parallel_image_loading);
-    }
-  else if (sys.totalram < GB(5)) {
-    property_override(PINNER_QUOTA, dhi->pinner_quota);
-    }
+    property_override(MADVISE_ARTFILE_SIZE_PROP, dhi->madvise_artfile_size);
 }
